d44ab200c5a94d6f942194a4885fc2972d6ae1e7
Nicholas Derkach <nicholasgarfild@gmail.com>
2020-05-08 18:34:15 +0300

    Add project files.

---
+++ NavigationDrawerPopUpMenu2/UserControlCreate.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace NavigationDrawerPopUpMenu2
+{
+    /// <summary>
+    /// Interação lógica para UserControlCreate.xam
+    /// </summary>
+    public partial class UserControlCreate : UserControl
+    {
+        public UserControlCreate()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string path = "C:\\Users\\nicho\\OneDrive\\Робочий стіл";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            string text = "Електронна пошта: " + textBox1.Text + "\n П.І.Б. Студента: " + textBox2.Text;
+            using (FileStream fstream = new FileStream($"{path}\\lab7.txt", FileMode.OpenOrCreate))
+            {
+                byte[] array = Encoding.Default.GetBytes(text);
+                fstream.Write(array, 0, array.Length);
+            }
+        }
+    }
+}
